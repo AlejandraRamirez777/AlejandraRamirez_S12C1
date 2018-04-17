1a22eba0626c39357dd5effc87bbc71e8b8d6b24
AlejandraRamirez <ma.ramirez@uniandes.edu.co>
2018-04-17 18:22:24 -0500

    Adv

---
+++ AlejandraRamirez_S12C1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    int nX = 80;
+    int nT = 300;
+
+    double dt = 0.01;
+    double c = 1.0;
+
+    double U[nX];
+    double X[nX];
+
+    double cx = 0;
+    double dx = 2.0/80.0 ;
+    int ci = 0;
+    double L = 2.0;
+
+    while(cx<L){
+         X[ci] = cx;
+	 cx += cx*dx;
+         ci++;   
+    }
+
+    for(int i=0; i<L;i++){
+        if(X[i]<1.25 and X[i]>0.75){
+	    U[i] = 2.0;
+	}
+    	else{
+            U[i] = 0.0;
+    	}
+      cout << U[i] << endl;
+    }
+
+
+}
+
